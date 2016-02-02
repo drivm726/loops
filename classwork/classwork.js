88438190ea974b57b406c08a97bbe9f711e4c915
drivm726 <drivm726@newschool.edu>
2016-02-02 18:12:11 -0500

    Classwork
    Loops

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -3,15 +3,27 @@
 */
 
 var cellSize = 50;
+var cellX;
+var cellY;
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
+  for(var row=0; row<20;row++) {
 
-  for(var row = 0; row <20;row++){
+  for(var column=0; column <20;column++){
 
-    for(var column =0; column <20;column++){
+    stroke("black");
+    cellX = cellSize*column;
+    cellY = cellSize*row;
+    cellY2 = cellY/6;
+    fill(245,211-cellY2,244-(cellY2/2));
+    rect(cellX, cellY, cellSize, cellSize);
+    fill(245-cellY2,211-(cellY2*2),244-(cellY2/3));
 
-      rect(cellSize*column, cellSize*row, cellSize, cellSize);
+    triangle(cellX,random(cellY, cellY +cellSize),cellX+cellSize,cellY,cellX+cellSize,cellY+cellSize);
+
+
+
 
     }
   }
